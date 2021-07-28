04aa467be18507bfdc35eb3565724aeff0cccf59
halcom <okazawa.game@gmail.com>
2021-07-28 14:34:54 +0900

    プレイヤーの作成

---
+++ BooFarmPro/Assets/Scripts/PlayerCon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤー関連
+/// </summary>
+public class PlayerCon : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

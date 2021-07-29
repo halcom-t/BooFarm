e52430b61660aa8476cbe82d6ce68fd36ed8db74
halcom <okazawa.game@gmail.com>
2021-07-29 14:24:09 +0900

    タップと長押しの判定処理追加

--- BooFarmPro/Assets/Scripts/PlayerCon.cs
+++ BooFarmPro/Assets/Scripts/PlayerCon.cs
@@ -8,6 +8,21 @@ using UnityEngine;
 public class PlayerCon : MonoBehaviour
 {
 
+    //道具
+    enum ToolStatus{
+        None, 
+        Kuwa    //クワ
+    }
+
+    //タップした位置
+    Vector3 tapPos;
+
+    //長押し時間計測用タイマー
+    float longTapTimer;
+
+    //タップ判定時間
+    const float tapTime = 0.15f;
+
     void Start()
     {
         
@@ -15,16 +30,65 @@ public class PlayerCon : MonoBehaviour
 
     void Update()
     {
-        //スワイプ中
-        if (Input.GetMouseButton(0))
-        {
-            //タップしている位置を取得（画面中央 0,0）
-            float tapPosX = Input.mousePosition.x - Screen.width / 2;
-            float tapPosY = Input.mousePosition.y - Screen.height / 2;
 
-            //移動
-            transform.Translate(new Vector2(tapPosX, tapPosY).normalized * Time.deltaTime * 2);
+        //タップした時
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapPos = Input.mousePosition;
+            longTapTimer = 0f;
+        }
+
+        //離した時
+        if (Input.GetMouseButtonUp(0))
+        {
+            //タップならアクション実行
+            if (isTap())TapAction();
             
         }
+
+        //長押し中
+        if (Input.GetMouseButton(0))
+        {
+            //長押し時間の計測
+            longTapTimer += Time.deltaTime;
+            //移動
+            Move();        
+        }
     }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    void Move()
+    {
+        //タップなら移動処理をしない
+        if (isTap()) return;
+
+        //スワイプしている位置を取得（画面中央 0,0）
+        float swipePosX = Input.mousePosition.x - Screen.width / 2;
+        float swipePosY = Input.mousePosition.y - Screen.height / 2;
+
+        //移動
+        transform.Translate(new Vector2(swipePosX, swipePosY).normalized * Time.deltaTime * 2);
+    }
+
+    /// <summary>
+    /// タップによる各アクション
+    /// </summary>
+    void TapAction()
+    {
+
+    }
+
+    /// <summary>
+    /// タップか判定
+    /// ※現時点での長押しがタップに該当するかを判定する
+    /// </summary>
+    /// <returns>タップ=true</returns>
+    bool isTap()
+    {
+        //タップ　=　長押し時間がタップ判定時間以内　＆　タップ位置がずれていない
+        return longTapTimer <= tapTime && tapPos == Input.mousePosition;
+    }
+
 }
